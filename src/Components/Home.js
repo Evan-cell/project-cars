7102f55783c08fe8711d573848cb0321dd947d19
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-08-31 09:54:42 +0300

    logo

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,6 +3,13 @@ import '../Components/Home.css'
 function Home() {
   return (
     <div className='home'>
+        <div className="header">
+            <div className="header__left">
+                <img src="https://www.logo.wine/a/logo/Tesla%2C_Inc./Tesla%2C_Inc.-Wordmark-Black-Logo.wine.svg" alt="" className='img__header'/>
+            </div>
+            <div className="header__center"></div>
+            <div className="header__right"></div>
+        </div>
     </div>
   )
 }
