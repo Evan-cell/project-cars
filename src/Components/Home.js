e029d8285f267ec322ae90e9d6ee66690e68ccfb
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-08-31 10:06:22 +0300

    header center

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,7 +7,14 @@ function Home() {
             <div className="header__left">
                 <img src="https://www.logo.wine/a/logo/Tesla%2C_Inc./Tesla%2C_Inc.-Wordmark-Black-Logo.wine.svg" alt="" className='img__header'/>
             </div>
-            <div className="header__center"></div>
+            <div className="header__center">
+                <a href="">Model S</a>
+                <a href="">Model 3</a>
+                <a href="">Model X</a>
+                <a href="">Model Y</a>
+                <a href="">Solar Roof</a>
+                <a href="">Solar Panels</a>
+            </div>
             <div className="header__right"></div>
         </div>
     </div>
